900e86953a856349ed1ddc47edfd8778b93fa00d
grudzug <grudzug@gmail.com>
2022-09-25 01:02:18 +0200

    feat: add statemanagement (getx)

---
+++ lib/controllers/users_controller.dart
@@ -0,0 +1,78 @@
+import 'package:get/get.dart';
+import 'package:mygetxapp/models/user_model.dart';
+import 'package:mygetxapp/services/api.dart';
+
+class UsersController extends GetxController {
+  var isLoaded = false.obs;
+  var errorMessage = ''.obs;
+  final users = <User>[].obs;
+  Api api = Api(url: 'https://assessment-users-backend.herokuapp.com/users/');
+
+  @override
+  void onInit() {
+    fetchUsers();
+    super.onInit();
+  }
+
+  void fetchUsers() async {
+    try {
+      isLoaded.value = false;
+      final userList = await api.getUsers();
+      if (userList != null) {
+        users.value = userList;
+      }
+    } catch (error) {
+      errorMessage.value = error.toString();
+    } finally {
+      isLoaded.value = true;
+    }
+  }
+
+  void updateUserStatus(userToUpdate) async {
+    var isActive = userToUpdate.status == 'active';
+    var newStatus = isActive ? 'locked' : 'active';
+
+    try {
+      isLoaded.value = true;
+      await api.updateStatus(userToUpdate.id, newStatus);
+    } catch (error) {
+      errorMessage.value = error.toString();
+    } finally {
+      fetchUsers();
+    }
+  }
+
+  void editUser(userToUpdate, newFirstName, newLastName) async {
+    try {
+      isLoaded.value = true;
+      await api.editUser(userToUpdate.id, newFirstName, newLastName);
+    } catch (error) {
+      errorMessage.value = error.toString();
+    } finally {
+      fetchUsers();
+    }
+  }
+
+  void addUser(firstName, lastName) async {
+    try {
+      isLoaded.value = false;
+      await api.addUser(firstName, lastName);
+    } catch (error) {
+      errorMessage.value = error.toString();
+    } finally {
+      fetchUsers();
+    }
+  }
+
+  void deleteUser(userToDelete) async {
+    try {
+      isLoaded.value = false;
+      await api.deleteUser(userToDelete.id);
+      users.removeWhere((user) => user.id == userToDelete.id);
+    } catch (error) {
+      errorMessage.value = error.toString();
+    } finally {
+      fetchUsers();
+    }
+  }
+}
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,147 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:mygetxapp/models/user_model.dart';
-import 'package:mygetxapp/services/api.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/instance_manager.dart';
+import 'package:mygetxapp/controllers/users_controller.dart';
 import 'package:mygetxapp/views/user_card.dart';
-import 'package:mygetxapp/views/user_creation_form.dart';
 
 class UserList extends StatefulWidget {
-  const UserList({super.key});
+  const UserList({Key? key}) : super(key: key);
 
   @override
   State<UserList> createState() => _UserListState();
 }
 
 class _UserListState extends State<UserList> {
-  List<User>? users = [];
-  bool isLoaded = false;
-  Api api = Api(url: 'https://assessment-users-backend.herokuapp.com/users/');
-
-  @override
-  void initState() {
-    super.initState();
-    fetchUsers();
-  }
-
-  void updateUserStatus(userToUpdate) async {
-    setState(() {
-      isLoaded = false;
-    });
-
-    var isActive = userToUpdate.status == 'active';
-    var newStatus = isActive ? 'locked' : 'active';
-
-    try {
-      await api.updateStatus(userToUpdate.id, newStatus);
-      setState(() {
-        users!.forEach((user) {
-          if (user.id == userToUpdate.id) {
-            user.status = newStatus;
-          }
-        });
-      });
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('$error'),
-        ),
-      );
-    } finally {
-      setState(() {
-        isLoaded = true;
-      });
-    }
-  }
-
-  void editUser(userToUpdate, newFirstName, newLastName) async {
-    setState(() {
-      isLoaded = false;
-    });
-
-    try {
-      await api.editUser(userToUpdate.id, newFirstName, newLastName);
-      setState(() {
-        users!.forEach((user) {
-          if (user.id == userToUpdate.id) {
-            user.firstName = newFirstName;
-            user.lastName = newLastName;
-          }
-        });
-      });
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('$error'),
-        ),
-      );
-    } finally {
-      setState(() {
-        isLoaded = true;
-      });
-    }
-  }
-
-  void addUser(firstName, lastName) async {
-    setState(() {
-      isLoaded = false;
-    });
-
-    try {
-      await api.addUser(firstName, lastName);
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('$error'),
-        ),
-      );
-    } finally {
-      fetchUsers();
-    }
-  }
-
-  void deleteUser(userToDelete) async {
-    setState(() {
-      isLoaded = false;
-    });
-
-    try {
-      await api.deleteUser(userToDelete.id);
-      setState(() {
-        users!.removeWhere((user) => user.id == userToDelete.id);
-      });
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('$error'),
-        ),
-      );
-    } finally {
-      setState(() {
-        isLoaded = true;
-      });
-    }
-  }
-
-  void fetchUsers() async {
-    setState(() {
-      isLoaded = false;
-    });
-    try {
-      final users = await api.getUsers();
-      setState(() {
-        this.users = users;
-      });
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('$error'),
-        ),
-      );
-    } finally {
-      setState(() {
-        isLoaded = true;
-      });
-    }
-  }
+  final UsersController usersController = Get.put(UsersController());
 
   @override
   Widget build(BuildContext context) {
@@ -151,37 +22,25 @@ class _UserListState extends State<UserList> {
         centerTitle: true,
         backgroundColor: Colors.green[600],
       ),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          fetchUsers();
+      body: Obx(
+        () {
+          if (usersController.isLoaded.value == false) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: usersController.users.length,
+            itemBuilder: (context, index) {
+              return UserCard(
+                user: usersController.users[index],
+                update: usersController.updateUserStatus,
+                edit: usersController.editUser,
+                delete: usersController.deleteUser,
+              );
+            },
+          );
         },
-        child: isLoaded
-            ? ListView.builder(
-                itemCount: users!.length,
-                itemBuilder: (context, index) {
-                  return UserCard(
-                    user: users![index],
-                    update: updateUserStatus,
-                    edit: editUser,
-                    delete: deleteUser,
-                  );
-                },
-              )
-            : const Center(
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.green,
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => UserCreationForm(mutate: addUser)));
-        },
-        child: const Icon(
-          Icons.add,
-        ),
       ),
     );
   }
